785d3fc75db35e5dce614ebc16db34fd042d05c6
Samuel Pecár <samuel.pecar@hotmail.com>
2019-01-27 13:43:24 +0100

    RNN encoder - only LSTM support

---
+++ modules/layers/encoders/rnn_encoder.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+
+
+# TODO - only LSTM add support for type GRU
+class RNNEncoder(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers=1, bidirectional=True, dropout=0.3, type='LSTM', **kwargs):
+        super(RNNEncoder, self).__init__()
+
+        self.bidirectional = bidirectional
+        self.num_layers = num_layers
+        self.num_directions = 2 if self.bidirectional else 1
+
+        self.hidden_size = hidden_size
+        self.feature_size = self.num_directions * hidden_size
+
+        self.dropout = nn.Dropout(p=dropout)
+        self.encoder = nn.LSTM(input_size=input_size,
+                               hidden_size=hidden_size,
+                               num_layers=num_layers,
+                               bidirectional=bidirectional,
+                               dropout=dropout,
+                               batch_first=True)
+
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters())
+        return (weight.new_zeros(self.num_layers * self.num_directions, batch_size, self.hidden_size),
+                weight.new_zeros(self.num_layers * self.num_directions, batch_size, self.hidden_size))
+
+    def forward(self, embedded, hidden, mask, lengths):
+        packed_embedded = pack_padded_sequence(embedded, lengths, batch_first=True)
+
+        lstm_output, lstm_hidden = self.encoder(packed_embedded, hidden)
+
+        padded_output, _ = pad_packed_sequence(lstm_output, batch_first=True)
+
+        return self.dropout(padded_output), lstm_hidden
