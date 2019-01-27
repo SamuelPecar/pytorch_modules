276bfd34a7b3ef001bdfefe8d999150ba1b6ab13
Samuel Pecár <samuel.pecar@hotmail.com>
2019-01-27 13:31:32 +0100

    vocabulary class

---
+++ modules/common/vocabulary.py
@@ -0,0 +1,26 @@
+class Vocabulary(object):
+    """
+    Basic class representing dataset vocabulary
+    """
+
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.stats = {}
+        self.idx = 1
+
+    def process_tokens(self, text):
+        for token in text:
+            self.add_word(token)
+
+    def add_word(self, word):
+        if not word in self.word2idx:
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
+            self.stats[word] = 1
+        else:
+            self.stats[word] += 1
+
+    def __len__(self):
+        return len(self.word2idx)
