a8ea8180c42929f6962c2f76611efd0b4ec6b9ca
Samuel Pecár <samuel.pecar@hotmail.com>
2019-03-12 16:03:54 +0100

    vocabulary sorting function

--- modules/common/vocabulary.py
+++ modules/common/vocabulary.py
@@ -1,3 +1,6 @@
+import operator
+
+
 class Vocabulary(object):
     """
     Basic class representing dataset vocabulary
@@ -10,6 +13,7 @@ class Vocabulary(object):
 
         self.idx = 0
         self.add_word('<PAD>')
+        self.add_word('<EOS>')
 
     def process_tokens(self, text):
         for token in text:
@@ -26,3 +30,16 @@ class Vocabulary(object):
 
     def __len__(self):
         return len(self.word2idx) + 1
+
+    def sort(self):
+        self.stats = dict(sorted(self.stats.items(), key=operator.itemgetter(1), reverse=True))
+
+        sorted_word2idx = {}
+        sorted_idx = 0
+
+        for key in self.stats:
+            sorted_word2idx[key] = sorted_idx
+            sorted_idx += 1
+
+        self.word2idx = sorted_idx
+        self.idx2word = {v: k for k, v in sorted_idx.items()}
