4f4c44046be33c1f6c8a67163bee51aa7fc91a6b
Samuel Pecár <samuel.pecar@hotmail.com>
2019-11-04 13:37:07 +0100

    support for ELMO for many langs

---
+++ modules/layers/embeddings/elmo_many_langs.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from elmoformanylangs import Embedder
+from torch.nn.utils.rnn import pad_sequence
+
+from config import device
+
+class ELMoForManyLangs(nn.Module):
+    """
+    """
+
+    def __init__(self, embedding_dropout=0., emb_path='./ELMo', **kwargs):
+        super(ELMoForManyLangs, self).__init__()
+
+        self.dropout = nn.Dropout(p=embedding_dropout)
+        self.embedder = Embedder(emb_path)
+        self.embedding_dim = 1024
+
+    def forward(self, sentences):
+        embedded = self.embedder.sents2elmo(sentences)
+
+        embedded_list = []
+        masks = []
+
+        for e in embedded:
+            embedded_list.append(self.dropout(torch.Tensor(e)))
+            masks.append(torch.ones(len(e)).long())
+
+        return pad_sequence(embedded_list, batch_first=True).to(device), pad_sequence(masks, batch_first=True).to(device)
