1b062ab0262ea772e9b325446b082def5475c928
Samuel Pecár <samuel.pecar@hotmail.com>
2019-11-12 21:18:20 +0100

    removing non-generic collate fn

--- modules/common/dataloading/collate.py
+++ modules/common/dataloading/collate.py
@@ -19,17 +19,3 @@ def collate_fn_cf(data):
         samples = samples
 
     return samples, labels, lengths
-
-
-def collate_fn_IMN(data):
-    samples, aspects, polarities, opinions, lengths = zip(*data)
-
-    opinions = torch.LongTensor(opinions)
-    lengths = torch.LongTensor(lengths)
-
-    if isinstance(samples[0], np.ndarray):
-        samples = torch.LongTensor(samples)
-    elif isinstance(samples[0], tuple):
-        samples = samples
-
-    return samples, list(aspects), list(polarities), opinions, lengths
